bba9b70fdd61f6db6ffb9b28f69ba6b425217fbd
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2023-05-02 18:41:41 +0200

    feat: models creation

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
