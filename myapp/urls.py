09b73fbd0f0ce54b7bab52cc264c8e81fdfbc8f6
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2023-05-04 22:15:36 +0200

    url params creation

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.index),
+    path('salute/<str:username>', views.hello),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request) -> HttpResponse:
-    return HttpResponse("<h1> Hola Buenas </h1>")
+def hello(request, username: str) -> HttpResponse:
+    return HttpResponse(f"<h1> Hola Buenas {username} </h1>")
+
+def index(request) -> HttpResponse:
+    return HttpResponse("<h1> Tremendo Indice Maquina </h1>")
