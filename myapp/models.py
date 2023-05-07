b3bd9e6d7567d63b769dc2c5175a88614401800b
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2023-05-07 18:56:07 +0200

    feat: admin site register models

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,12 @@ class Subject(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self) -> str:
+        return self.name
+
 class Student(models.Model):
     name = models.CharField(max_length=200)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
